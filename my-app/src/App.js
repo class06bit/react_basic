5d215090e5d41b86488c2a2cf4cd29be0b3be72d
Lee HyunJoo <wayhome250@gmail.com>
2017-09-25 21:56:15 +0900

    practice this.setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,31 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "http://leemisong.com/wp-content/uploads/2012/04/c1.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:  "http://static.rogerebert.com/uploads/movie/movie_poster/full-metal-jacket-1987/large_bleZBRX8XH6e9PR00aGCvdjvu3Q.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster:  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/May_the_4th_be_with_you_%28Star_Wars_Day%29.gif/240px-May_the_4th_be_with_you_%28Star_Wars_Day%29.gif"
-  }
-]
 
 class App extends Component {
 
   state = {
-    greeting: 'Hellllo!'
+    movies: [
+      {
+        title: "Matrix",
+        poster: "http://leemisong.com/wp-content/uploads/2012/04/c1.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster:  "http://static.rogerebert.com/uploads/movie/movie_poster/full-metal-jacket-1987/large_bleZBRX8XH6e9PR00aGCvdjvu3Q.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster:  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/May_the_4th_be_with_you_%28Star_Wars_Day%29.gif/240px-May_the_4th_be_with_you_%28Star_Wars_Day%29.gif"
+      }
+    ]
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        greeting: 'hello again'
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Mommy",
+            poster: "https://media.vanityfair.com/photos/54bef79f023a8bfc4cad2ae9/master/w_900,c_limit/mommy-poster.jpg"
+          }
+        ]
       })
     }, 2000)
   }
@@ -34,8 +39,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>

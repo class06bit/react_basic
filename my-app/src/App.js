7fb25591f61693dcac8983b7d461e7ffda0b2c9d
Lee HyunJoo <wayhome250@gmail.com>
2017-09-25 22:20:18 +0900

    Loading State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,44 +4,40 @@ import Movie from './Movie';
 
 
 class App extends Component {
-
-  state = {
-    movies: [
-      {
-        title: "Matrix",
-        poster: "http://leemisong.com/wp-content/uploads/2012/04/c1.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster:  "http://static.rogerebert.com/uploads/movie/movie_poster/full-metal-jacket-1987/large_bleZBRX8XH6e9PR00aGCvdjvu3Q.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster:  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/May_the_4th_be_with_you_%28Star_Wars_Day%29.gif/240px-May_the_4th_be_with_you_%28Star_Wars_Day%29.gif"
-      }
-    ]
-  }
+  state = {}
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
           {
-            title: "Mommy",
-            poster: "https://media.vanityfair.com/photos/54bef79f023a8bfc4cad2ae9/master/w_900,c_limit/mommy-poster.jpg"
+            title: "Matrix",
+            poster: "http://leemisong.com/wp-content/uploads/2012/04/c1.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster:  "http://static.rogerebert.com/uploads/movie/movie_poster/full-metal-jacket-1987/large_bleZBRX8XH6e9PR00aGCvdjvu3Q.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster:  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/May_the_4th_be_with_you_%28Star_Wars_Day%29.gif/240px-May_the_4th_be_with_you_%28Star_Wars_Day%29.gif"
           }
         ]
       })
     }, 2000)
   }
 
+  _renderMovies = () => { 
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading..'}
       </div>
     );
   }

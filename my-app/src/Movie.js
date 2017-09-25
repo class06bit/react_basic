bf66aefcd44abac02f2298548e29940c7cc117b9
Lee HyunJoo <wayhome250@gmail.com>
2017-09-25 22:29:49 +0900

    stateless functional components

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -2,34 +2,29 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
 
-  static propTypes = {
-    title: propTypes.stirng,
-    poster: propTypes.string
-  }
-
-  render(){
-    return (
-      <div>
-      <MoviePoster poster={this.props.poster}/>
-      <h1>{this.props.title}</h1>
-      </div>
-    )
-  }
+function Movie({title, poster}){
+  return (
+    <div>
+    <MoviePoster poster={poster}/>
+    <h1>{title}</h1>
+    </div>
+  )
 }
 
-class MoviePoster extends Component{
+function MoviePoster({poster}){
+  return (
+    <img src={poster} alt="poster" />
+  )
+}
 
-  static propTypes = {
-    poster : propTypes.stirng
-  }
+Movie.propTypes = {
+  title: propTypes.string.isRequired,
+  poster: propTypes.string.isRequired
+}
 
-  render(){
-    return(
-     <img src={this.props.poster} alt="poster" />
-    )
-  }
+MoviePoster.propTypes = {
+  poster: propTypes.string.isRequired
 }
 
 export default Movie;
